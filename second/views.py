c0cba4b8113ac6a27d0ea0bdc7b35b8e61bb0fd9
jewel <jmbengal.mbstu@gmail.com>
2022-03-28 20:24:38 +0600

    viewsets on phone added

--- second/views.py
+++ second/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from second.models import Phone_number, Subscribe
+from second.serializers import PhoneSerializer
+from rest_framework import viewsets
+
 # Create your views here.
 
 def home(request):
     return HttpResponse('Hello World!!!')
+
+class phoneViewset(viewsets.ModelViewSet):
+	"""
+	This viewset automatically provides `list`, `create`, `retrieve`, `update` and `destroy` actions.
+	"""
+	serializer_class = PhoneSerializer
+	queryset = Phone_number.objects.all()
