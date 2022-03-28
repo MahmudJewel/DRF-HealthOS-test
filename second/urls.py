04765d3ab4a6881fbb3f37426a54fed365269628
jewel <jmbengal.mbstu@gmail.com>
2022-03-28 20:37:26 +0600

    subscribe viewset added

--- second/urls.py
+++ second/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from second.views import home, phoneViewset
+from second.views import home, phoneViewset, subscribeViewset
 
 # router 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('phone', phoneViewset, basename='vs')
+router.register('phone', phoneViewset, basename='phone')
+router.register('sub', subscribeViewset, basename='sub')
 
 urlpatterns = [
     path('', home, name='home'),
