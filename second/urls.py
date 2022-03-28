81c43cc8e6ddb0d8d25ae977277668b3e7312a83
jewel <jmbengal.mbstu@gmail.com>
2022-03-29 00:56:47 +0600

    company and company phone router added

--- second/urls.py
+++ second/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from second.views import home, phoneViewset, subscribeViewset
+from second.views import home, phoneViewset, subscribeViewset,CompanyViewset, Company_Phone_ListViewset
 
 # router 
 from rest_framework.routers import DefaultRouter
@@ -7,6 +7,8 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('phone', phoneViewset, basename='phone')
 router.register('sub', subscribeViewset, basename='sub')
+router.register('company', CompanyViewset, basename='company')
+router.register('company-phone', Company_Phone_ListViewset, basename='company')
 
 urlpatterns = [
     path('', home, name='home'),
