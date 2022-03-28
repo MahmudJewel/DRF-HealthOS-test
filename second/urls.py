c2c840c0ba186868ded620ae9117569ba161638c
jewel <jmbengal.mbstu@gmail.com>
2022-03-28 20:25:16 +0600

    routers added for api

--- second/urls.py
+++ second/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
-from second.views import home
+from second.views import home, phoneViewset
+
+# router 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('phone', phoneViewset, basename='vs')
+
 urlpatterns = [
     path('', home, name='home'),
+    path('', include(router.urls))
 ]
